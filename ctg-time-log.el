6e82623fd5ed1af75d39d61a2c4536914d7ffab2
Coutego <coutego@gmail.com>
2021-12-27 21:01:59 +0100

    create entry function

--- ctg-time-log.el
+++ ctg-time-log.el
@@ -7,13 +7,14 @@
 ;; Created: December 27, 2021
 ;; Modified: December 27, 2021
 ;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/pedroabelleiraseco/ctg-time-log
-;; Package-Requires: ((emacs "24.3"))
+;; Keywords: convenience outlines
+;; Homepage: https://github.com/coutego/ctg-time-log
+;; Package-Requires: ((emacs "27.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
+;;;
 ;; Org mode offers very advance capabilities for logging time spent on different
 ;; tasks and produce reports on it. Unfortunately, those are unusable for my use case,
 ;; so I decided to create a helper package for personal use to create prefefined
@@ -24,6 +25,10 @@
 
 (require 'cl-lib)
 (require 'json)
+(require 'dash)
+(require 'f)
+
+(defmacro comment (&rest _body)) ;; Clojure's comment macro
 
 (defvar ctg-time-log-timestamp-format "%Y.%m.%d - %H:%M:%S:%2N")
 
@@ -33,14 +38,17 @@
 An entry should be a plist with keys :project, :type, :title"
   entry) ;; FIXME Implement this function
 
-
-(cl-defun ctg-time-log--create-entry (&keys project type title)
+(cl-defun ctg-time-log--create-entry (&key project type title)
   (let* ((timestamp (format-time-string ctg-time-log-timestamp-format))
          (entry (list :timestamp timestamp
                       :project   project
                       :type      type
                       :title     title)))
     (json-encode entry)))
+
+(cl)
+(comment
+ (ctg-time-log--create-entry :project "EUCTP" :type "TASK" :title "Review the project plan"))
 
 
 (provide 'ctg-time-log)
